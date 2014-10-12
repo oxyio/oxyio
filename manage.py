b84ed0279ed6885f3aafb50c81e02d108103ec1e
Nick Barrett <nick@oxygem.com>
2014-10-12 15:25:05 +0100

    load_modules() before db tasks

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 # Oxypanel
 # File: manage.py
 # Desc: management commands!
+#       this file does not, by default, load all the modules/etc
+#       this is to prevent log messages appearing at the start for things like node_config
 
 import os
 import sys
@@ -17,11 +19,16 @@ if __name__ == '__main__':
     # Force unbuffered python
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-    # Run shell
+    # Shell
     if 'shell' in sys.argv:
         from boot import load_modules
         load_modules()
         manager.add_command('shell', Shell())
+
+    # Db
+    if 'db' in sys.argv:
+        from boot import load_modules
+        load_modules()
 
     # Run manager
     manager.run()
