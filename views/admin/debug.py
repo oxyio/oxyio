95487fc9097dda76153774a186121cf939538c0f
Nick Barrett <nick@oxygem.com>
2014-12-20 19:34:18 +0000

    Better, sorted url maps

--- views/admin/debug.py
+++ views/admin/debug.py
@@ -2,12 +2,14 @@
 # File: views/admin/debug.py
 # Desc: the debug view!
 
+from operator import itemgetter
+
 from flask import g
 
 import config
-from app import app, module_map, object_map
+from app import app, module_map, object_map, websocket_map, item_map, task_map
 from util.web.response import render_or_jsonify
-from util.user import permissions_required
+from util.web.user import permissions_required
 
 
 @app.route('/admin/debug', methods=['GET'])
@@ -19,14 +21,14 @@ def admin_debug():
         action='debug',
         modules=module_map,
         objects=object_map,
-        urls={
-            url: {
-                'endpoint': url.endpoint,
-                'methods': url.methods - {'OPTIONS', 'HEAD'}
-            }
+        websockets=websocket_map,
+        items=item_map,
+        tasks=task_map,
+        urls=sorted([
+            (url, url.endpoint, url.methods - {'OPTIONS', 'HEAD'})
             for url in app.url_map.iter_rules()
             if 'debugtoolbar' not in url.endpoint and 'debug_toolbar' not in url.endpoint
-        },
+        ], key=itemgetter(0)),
         configs={
             key: getattr(config, key)
             for key in dir(config)
