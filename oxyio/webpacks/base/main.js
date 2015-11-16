2f6855dccb0cca60d77b43bc1d847beba2e60100
Nick Barrett <nick@oxygem.com>
2015-11-16 16:43:51 +0000

    Replace local `multiselect.js` as superseded by `selected.js`.

--- oxyio/webpacks/base/main.js
+++ oxyio/webpacks/base/main.js
@@ -2,22 +2,25 @@
 // Files: webpacks/base/main.js
 // Desc: the root of the base webpack
 
-// Library styles
-require('./less/lib/multiselect.less');
+var selectedJS = require('selected.js');
 
 // Base Oxypanel less
 require('./less/base.less');
 require('./less/buttons.less');
 require('./less/forms.less');
 require('./less/header.less');
+require('./less/lists.less');
 require('./less/loader.less');
 require('./less/login.less');
 require('./less/messages.less');
 require('./less/subheader.less');
 require('./less/tables.less');
-require('./less/lists.less');
 
 // Base Oxypanel js
-require('./js/lib/multiselect.js');
 require('./js/tabs.js');
 require('./js/tasks.js');
+
+// Convert multiselects into something usable with selected.js
+window.addEventListener('load', function() {
+    selectedJS.init('[multiple]');
+});
