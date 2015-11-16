d3a74578d17b02866b1926ae8e7d2b9c54330913
Nick Barrett <nick@oxygem.com>
2015-11-16 16:44:28 +0000

    Unsubscribe from pubsub after websocket closes.

--- oxyio/websockets/task.py
+++ oxyio/websockets/task.py
@@ -4,31 +4,35 @@
 
 from .. import settings
 from ..models.websocket import Websocket
-from ..util.web.pubsub import subscribe
+from ..util.web.pubsub import subscribe, unsubscribe
 
 
 class Subscribe(Websocket):
     '''Subscribes to a task via Redis pubsub & relays to client.'''
+
     NAME = 'core/task_subscribe'
 
     def __init__(self, data, ws):
         self.ws = ws
 
-        # Subscribe to the task
-        subscribe(self._on_data, channel='{}{}'.format(
-            settings.REDIS_TASK_PREFIX,
-            data
-        ))
+        channel_name = '{0}{1}'.format(settings.REDIS_TASK_PREFIX, data)
 
-        # Keeps the websocket open
+        # Subscribe to the task
+        subscribe(self.on_task_event, channel=channel_name)
+
+        # Wait until the websocket is closed
         self.run_forever()
 
-    def _on_data(self, data):
+        # Connection closed, let's unsusbscribe our function
+        unsubscribe(self.on_task_event, channel=channel_name)
+
+    def on_task_event(self, data):
         '''Forward incoming Redis events -> websocket, already JSON'd.'''
-        print data
+
         self.ws.send(data)
 
 
 class Admin(Websocket):
     '''Monitors & admins all running tasks.'''
+
     NAME = 'core/task_admin'
