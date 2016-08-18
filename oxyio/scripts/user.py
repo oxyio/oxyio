74dc12be1bb9b5a672ac8b8cfb0460c0505ab428
Nick Barrett <nick@oxygem.com>
2016-08-17 17:25:50 -0700

    Remove add_keymaster manage function.

--- oxyio/scripts/user.py
+++ oxyio/scripts/user.py
@@ -6,30 +6,18 @@ from oxyio.app import db, manager
 from oxyio.models.user import User
 
 
-def _add_user(is_keymaster=False):
+@manager.command
+def add_user():
+    '''
+    Add users.
+    '''
+
     email = raw_input('# Email address: ')
     password = raw_input('# Password: ')
 
     user = User(email, password)
-    user.is_keymaster = is_keymaster
 
     db.session.add(user)
     db.session.commit()
 
-    return email
-
-
-@manager.command
-def add_user():
-    '''Add normal users.'''
-
-    email = _add_user()
     print '# User {0} added!'.format(email)
-
-
-@manager.command
-def add_keymaster():
-    '''Add keymaster users (access-all-areas).'''
-
-    email = _add_user(is_keymaster=True)
-    print '# Keymaster {0} added!'.format(email)
